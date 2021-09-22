845855c2b39ef59276ba8a6c32d021e63020a119
lsm <chgt186@gmail.com>
2021-09-22 20:34:25 +0900

    board생성

---
+++ src/main/java/com/cos/lsm/domain/board/Board.java
@@ -0,0 +1,31 @@
+package com.cos.lsm.domain.board;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.cos.lsm.domain.user.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Board {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id; //PK (자동증가 번호)
+	private String title; // 아이디
+	private String content;
+	
+	@JoinColumn(name = "userId")
+	@ManyToOne
+	private User user;
+	
+}
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -3,8 +3,23 @@
     
 <%@ include file="/WEB-INF/views/layout/header.jsp" %>
 
+<div class="container">
+	<c:forEach begin="0" end="2">
+		<!-- 카드 글 시작 -->
+		<div class="card">
+			<div class="card-body">
+				<h4 class="card-title">Title 부분입니다.</h4>
+				<a href="#" class="btn btn-primary">상세보기</a>
+			</div>
+		</div>
+		<br />
+		<!-- 카드 글 끝 -->
+	</c:forEach>
 
-<h1>${sessionScope.principal.username}</h1>
-
+	<ul class="pagination d-flex justify-content-center">
+		<li class="page-item disabled"><a class="page-link" href="#">Prev</a></li>
+		<li class="page-item"><a class="page-link" href="#">Next</a></li>
+	</ul>
+</div>
 
 <%@ include file="/WEB-INF/views/layout/footer.jsp" %>
