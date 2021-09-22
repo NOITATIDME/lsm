77e022c46c26c7a286fb06bb571b7494f1fee7dc
lsm <chgt186@gmail.com>
2021-09-22 20:02:52 +0900

    @ResponseBody, Script, back 완료

--- src/main/java/com/cos/lsm/web/UserController.java
+++ src/main/java/com/cos/lsm/web/UserController.java
@@ -12,9 +12,11 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cos.lsm.domain.user.User;
 import com.cos.lsm.domain.user.UserRepository;
+import com.cos.lsm.util.Script;
 import com.cos.lsm.web.dto.JoinReqDto;
 import com.cos.lsm.web.dto.LoginReqDto;
 
@@ -36,7 +38,7 @@ public class UserController {
 	}
 	
 	@PostMapping("/join")
-	public String join(@Valid JoinReqDto dto, BindingResult bindingResult, Model model) { 
+	public @ResponseBody String join(@Valid JoinReqDto dto, BindingResult bindingResult, Model model) { 
 		System.out.println("에러사이즈 : " + bindingResult.getFieldErrors().size());
 		
 		if(bindingResult.hasErrors()) {
@@ -46,12 +48,39 @@ public class UserController {
 				System.out.println("필드 : " + error.getField());
 				System.out.println("메시지 : " + error.getDefaultMessage());
 			}
-			model.addAttribute("errorMap", errorMap);
-			return "error/error";
+			
+			return Script.back(errorMap.toString());
 		}
 		
 		userRepository.save(dto.toEntity());
-		return "redirect:/loginForm"; // 리다이렉션 (300)
+		return Script.href("/loginForm");
+	}
+
+	@PostMapping("/login")
+	public @ResponseBody String login(@Valid LoginReqDto dto, BindingResult bindingResult, Model model) {
+
+			System.out.println("에러사이즈:" + bindingResult.getFieldErrors().size());
+			
+			if( bindingResult.hasErrors() ) {
+				Map<String, String> errorMap = new HashMap<>();
+				for(FieldError error : bindingResult.getFieldErrors()) {
+					errorMap.put(error.getField(), error.getDefaultMessage());
+					System.out.println("필드:" + error.getField());
+					System.out.println("메시지:" + error.getDefaultMessage());
+				}
+				
+				return Script.back(errorMap.toString());
+			} 
+			
+				User userEntity =  userRepository.mLogin(dto.getUsername(), dto.getPassword());
+
+				if(userEntity == null) {
+					return Script.back("아이디 혹은 비밀번호를 잘못 입력하였습니다. 다시 입력하세요");
+				}else {
+					session.setAttribute("principal", userEntity);
+					return Script.href("/", "로그인 성공");					
+				}
+
 	}
 	
 	@GetMapping("/joinForm")
@@ -62,36 +91,6 @@ public class UserController {
 	@GetMapping("/loginForm")
 	public String loginForm() {
 		return "user/loginForm";
-	}
-	
-
-	@PostMapping("login")
-	public String login(@Valid LoginReqDto dto, BindingResult bindingResult, Model model) {
-
-			User userEntity =  userRepository.mLogin(dto.getUsername(), dto.getPassword());
-			
-			System.out.println("에러사이즈:" + bindingResult.getFieldErrors().size());
-			
-			if( bindingResult.hasErrors() ) {
-				Map<String, String> errorMap = new HashMap<>();
-				for(FieldError error : bindingResult.getFieldErrors()) {
-					errorMap.put(error.getField(), error.getDefaultMessage());
-					System.out.println("필드:" + error.getField());
-					System.out.println("메시지:" + error.getDefaultMessage());
-				}
-				model.addAttribute("errorMap", errorMap);
-				return "error/error";
-			} 
-
-
-
-		
-				session.setAttribute("principal", userEntity);
-				return "redirect:/home";
-
-
-
-
 	}
 
 	
